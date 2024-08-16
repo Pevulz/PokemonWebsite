0194c8e68a39a8b365731f35db065e38f581a98b
Tony Li <tonyli7119@gmail.com>
2024-08-16 16:25:12 -0400

    setup tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-green-300 text-3xl font-bold underline">
+        Hello world!
+      </div>
+    </>
+  );
+}
+
+export default App;
